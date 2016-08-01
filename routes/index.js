ec5a7e62a95b24fc18564d1482cb0897c3a87c33
MIchael <michael.olson756@gmail.com>
2016-08-01 15:44:30 -0600

    splash page

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('splashpage')
 });
-router.get('/signup', function(req, res, next){
-  res.render('signup')
+router.get('/login', function(req, res, next){
+  res.render('login')
 })
+
+
+
 module.exports = router;
