434bdf0ac11ba1c25d5c74c9222c03163bbfdd80
amespineira <amespineira@me.com>
2016-08-02 10:34:39 -0600

    doing routes

--- routes/beer.js
+++ routes/beer.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Beer=require('../queries/beer.js')
+var defaults=[
+  {
+    type:'ale',
+    stlye:'dark',
+    description:'what a thing',
+    ingredients:[1,2]
+  }
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('logged in as '+req.session.id);
@@ -11,9 +19,16 @@ router.get('/', function(req, res, next) {
 })
 router.get('/:id', function(req, res, next){
   Beer.getOne(req.params.id).then(function(beers){
-    res.send(beers)
+    if(beers.rows[0].user_id===req.session.id){
+    res.render('beer/showbeer', {beers:beers.rows})
+    }
+    else{
+      res.send('not your beer, get out of here')
+    }
   })
 })
-
+router.get('/create', function(req, res, next){
+  res.render('beer/select', {defaults:defaults})
+})
 
 module.exports = router;
