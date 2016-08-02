1226c87e0f42022606538f2422982de53ce27d41
jbwg312 <jbwg312@gmail.com>
2016-08-02 10:34:46 -0600

    figuring out batch route

--- routes/batch.js
+++ routes/batch.js
@@ -12,14 +12,22 @@ function Beer_ingredients(){
   return knex('beer_ingredients');
 }
 
+router.post('/time', function(req, res, next){
+  console.log(req.body);
+})
+
 router.post('/:id', function(req, res, next){
   //will be req.params.id instead of hardcode
   Beer().where({id: req.params.id}).select().then(function(beer){
     knex.raw(`INSERT into batch values(default, ${req.session.id}, ${beer[0].user_id}, ${beer[0].id}, default, )`)
       console.log(beer);
   })
-  console.log(req.session.id);
 })
+
+router.get('/', function(req, res, next){
+  res.render('batch/show')
+})
+
 
 
 
