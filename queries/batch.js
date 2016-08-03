e0640ed119995780eef5b53601cc11e40511d4ba
jbwg312 <jbwg312@gmail.com>
2016-08-03 10:25:08 -0600

    Batch branch up to notes

--- queries/batch.js
+++ queries/batch.js
@@ -4,10 +4,16 @@ module.exports = {
   createBatch: function(body, id1, id2){
     return knex.raw(`INSERT into batch values(default, ${id1}, ${id2}, ${Date()}, ${body.end_date}, ${body.expected_yeild}, ${null}, ${1});`)
   },
+  beer_id: function(id){
+    return knex.raw(`SELECT beer_id from batch where id=${id}`);
+  },
   equiptment: function(id){
     return knex.raw(`SELECT * from equipment where batch_id = ${id};`)
   },
   brewer_notes: function(id){
     return knex.raw(`SELECT notes from brewer_notes where beer_id = ${id};`)
+  },
+  add_notes: function(user, beer, text){
+    return knex.raw(`INSERT into brewer_notes values(default, ${user}, ${beer}, '${text}');`)
   }
 }
--- routes/batch.js
+++ routes/batch.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 var Queries_batch = require('../queries/batch');
+var Beer = require('../queries/beer')
+var Ing=require('../queries/ingredients.js')
 
 function Batch(){
   return knex('batch');
@@ -30,15 +32,27 @@ router.get('/create/:id', function(req, res, next){
   res.render('batch/form')
 })
 
+router.post('/notes/:id', function(req, res, next){
+  Queries_batch.add_notes(req.params.id, req.body.beer_id, req.body.notes).then(function(){
+    res.redirect(`/batch/${req.body.batch_id}`)
+  })
+})
+
 router.get('/:id', function(req, res, next){
-    Queries_batch.equiptment(req.params.id).then(function(equip){
-      Queries_batch.brewer_notes(data.rows[0].beer_id).then(function(notes){
-        if(data.rows.length === 0){
-          res.redirect('/beer')
-        }else{
-          res.render('batch/show', {batch: data.rows, equipment: equip.rows, notes: notes.rows})
-        }
+  Queries_batch.beer_id(req.params.id).then(function(id){
+    Promise.all([Beer.getOne(id.rows[0].beer_id),Ing.getBeersIng(id.rows[0].beer_id)]).then(function(results){
+      Queries_batch.equiptment(req.params.id).then(function(equip){
+        Queries_batch.brewer_notes(id.rows[0].beer_id).then(function(notes){
+          if(results[0].rows.length === 0){
+            res.redirect('/beer')
+          }else if (results[0].rows[0].user_id === req.session.id){
+            res.render('batch/show', {beer: results[0].rows, ing: results[1].rows, equipment: equip.rows, notes: notes.rows})
+          } else {
+            res.redirect('/');
+          }
+        })
       })
+    })
   })
 })
 
