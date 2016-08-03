72ea68cc826b59c735b071ed8dfb99ca015a5166
Alex Black <alexjblack322@gmail.com>
2016-08-03 14:00:57 -0600

    All crud routes minimally working

--- queries/equipment.js
+++ queries/equipment.js
@@ -9,5 +9,11 @@ module.exports={
   },
   findOneEquipment: function(id){
     return knex.raw(`SELECT * FROM equipment WHERE id= ${id}`)
+  },
+  updateOneEquipment: function(equip, id){
+    return knex.raw(`UPDATE equipment SET equipment_name='${equip.equipment_name}' , batch_id=${equip.batch_id} , clean='${equip.clean}' , clean_time='${equip.cleaning_time}' , clean_notes='${equip.cleaning_notes}' WHERE id=${id}; `)
+  },
+  deleteOneEquipment: function(id){
+    return knex.raw(`DELETE FROM equipment WHERE id=${id}`)
   }
 }
