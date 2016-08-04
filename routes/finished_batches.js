6823bc9f3d8f5dec05d38bc18ad81cfbf66ee5db
MIchael <michael.olson756@gmail.com>
2016-08-04 13:54:50 -0600

    finished the beer type on the finished_batches page

--- routes/finished_batches.js
+++ routes/finished_batches.js
@@ -6,8 +6,7 @@ router.get('/:batchId', function(req, res, next){
   console.log(req.params.batchId)
   Finished.all(req.params.batchId).then(function(finished_batch){
     console.log(finished_batch.rows)
-    res.render('finished_batch/index', {finished_batch: finished_batch.rows})
-
+    res.render('finished_batch/index', {finis  hed_batch: finished_batch.rows[0]})
   })
 });
 
