de1e35e22e26265be0306a5e0b53b4e2dfc3d0dd
MIchael <michael.olson756@gmail.com>
2016-08-04 10:52:37 -0600

    finished batches migration and table finished

--- migrations/20160804103233_finished_batches.js
+++ migrations/20160804103233_finished_batches.js
@@ -1,9 +1,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('finished_batches', function(table){
     table.increments();
-    table.string('beer_name')
-    table.integer('user_id')
+    table.string('beer_name');
+    table.integer('user_id');
     table.foreign('user_id').references('id').inTable('users');
+    table.integer('batch_id');
+    table.foreign('batch_id').references('id').inTable('batch');
     table.string('taste');
     table.string('aftertaste');
     table.string('appearance');
