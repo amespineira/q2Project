fec9198382b805ba61915e2647e59eb5c3f68692
amespineira <amespineira@me.com>
2016-08-05 09:43:54 -0600

    fixed queries

--- queries/ingredients.js
+++ queries/ingredients.js
@@ -15,35 +15,47 @@ module.exports={
     var curr=ingredients.pop()
     var query=`SELECT * FROM ingredients WHERE ingredient_name='${curr.name}' AND ingredient_type='${curr.type}' AND units='${curr.units}'`
     var insert=`INSERT INTO ingredients VALUES (DEFAULT, '${curr.name}', '${curr.type}', '${curr.units}', 'No description')`
-    knex.raw(query).then(function(matches){
+    return knex.raw(query).then(function(matches){
       if(matches.rows.length===0){
         knex.raw(insert).then(function(){
+          return (ingredients.length>0)? this.createIfMissing(ingredients) : Promise.resolve(true);
         })
       }
+      else{
+        return (ingredients.length>0)? this.createIfMissing(ingredients) : Promise.resolve(true);
+      }
     })
-    return (ingredients.length>0)? this.createIfMissing(ingredients) : Promise.resolve(true);
   },
+
   createBeerIngredients:function(ingredients, beerid){
     var curr=ingredients.pop()
     var query=`SELECT * FROM ingredients WHERE ingredient_name='${curr.name}' AND ingredient_type='${curr.type}' AND units='${curr.units}'`
-    knex.raw(query).then(function(matches){
+    return  knex.raw(query).then(function(matches){
       console.log(matches);
-      knex.raw(`INSERT INTO beer_ingredients VALUES (${beerid}, ${matches.rows[0].id}, ${curr.amount})`)
+      return knex.raw(`INSERT INTO beer_ingredients VALUES (DEFAULT, ${beerid}, ${matches.rows[0].id}, ${curr.amount})`).then(function(){
+        return (ingredients.length>0)? this.createBeerIngredients(ingredients, beerid) : Promise.resolve(true);
+      })
     })
-    return (ingredients.length>0)? this.createBeerIngredients(ingredients, beerid) : Promise.resolve(true);
   },
   createBITest2:function(ingredients, beerid){
     //get id of ingredient,create beer ingredient
+    console.log("in bites2");
     var promiseArray=[];
     for(var i=0; i<ingredients.length; i++){
+      console.log("createing promise "+i);
       promiseArray[i]=knex.raw(`SELECT * FROM ingredients WHERE ingredient_name='${ingredients[i].name}' AND ingredient_type='${ingredients[i].type}' AND units='${ingredients[i].units}'`)
     }
     return Promise.all(promiseArray).then(values => {
+      console.log("first array complete");
       var pa2=[]
       for(var i=0; i<values.length; i++){
-        pa2[i]=knex.raw(`INSERT INTO beer_ingredients VALUES (DEFAULT, ${beerid}, ${values[i].rows[0].id}, ${ingredients[i].amount})`)
+        console.log("creating second promise array");
+        pa2[i]=knex.raw(`INSERT INTO beer_ingredients VALUES (DEFAULT, ${beerid}, ${values[i].rows[0].id}, ${ingredients[i].amount})`);
+        console.log("at line 49");
       }
+      console.log("finished second array");
       return Promise.all(pa2).then(values => {
+        console.log("second done");
         return 'done'
       })
     });
