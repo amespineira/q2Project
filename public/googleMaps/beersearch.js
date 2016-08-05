67ba7cef4387eb69d0d5fd8260a663bd5c788367
MIchael <michael.olson756@gmail.com>
2016-08-05 15:10:51 -0600

    beer search complete

--- public/googleMaps/beersearch.js
+++ public/googleMaps/beersearch.js
@@ -1,20 +1,42 @@
 var userInput2 = document.getElementById('inputBeer1')
 var button2 = document.getElementById('buttonBeer1');
+var container = document.getElementById('container')
+var container2 = document.getElementById('container2')
+var container3 = document.getElementById('container3')
 var httpRequest = new XMLHttpRequest();
 
 
 button2.addEventListener("click", function(event) {
+    container.innerHTML = " "
   httpRequest.onreadystatechange = function(){
   if(httpRequest.readyState === 4){
     if(httpRequest.status < 400){
       var object = JSON.parse(httpRequest.responseText)
-      console.log(object.data);
+      for (var i = 0; i < object.data.length; i++) {
+      var name = document.createElement("h1")
+      var desc = document.createElement("h4")
+      var fgMax = document.createElement("li")
+      var fgMin = document.createElement("li")
+      var ibuMax = document.createElement('li')
+      var ibuMin = document.createElement('li')
+      var abv = document.createElement('li')
+      var glass = document.createElement('li')
 
-      // console.log(object.data[0].name);
-      // console.log(object.data[0].style.description);
-      // console.log(object.data[0].style.ibuMax);
-      // console.log(object.data[0].style.ibuMin);
-
+      container.appendChild(name)
+      name.innerHTML = object.data[i].style.name;
+      container.appendChild(desc)
+      desc.innerHTML = object.data[i].style.description;
+      container.appendChild(fgMax)
+      fgMax.innerHTML = 'FgMax'+ " " + object.data[i].style.fgMax;
+      container.appendChild(fgMin)
+      fgMin.innerHTML = 'FgMin' + " " + object.data[i].style.fgMin;
+      container.appendChild(ibuMax)
+      ibuMax.innerHTML = 'Ibu Max' + " " + object.data[i].style.ibuMax;
+      container.appendChild(ibuMin)
+      ibuMin.innerHTML = 'Ibu Min' + " " + object.data[i].style.ibuMin;
+      container.appendChild(abv)
+      abv.innerHTML = 'ABV' + " " + object.data[i].abv;
+          }
         }
       }
     }
