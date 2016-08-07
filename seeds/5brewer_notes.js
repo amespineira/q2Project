31b059b8ece385e7042719531957528b67590144
Alex Black <alexjblack322@gmail.com>
2016-08-07 12:50:07 -0600

    Successful case seed files for user AlexTheBlackBeard

--- seeds/5brewer_notes.js
+++ seeds/5brewer_notes.js
@@ -5,22 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('brewer_notes').insert({user_id:1 , beer_id:1 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:1 , beer_id:1 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:1 , beer_id:2 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:1 , beer_id:2 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:2 , beer_id:3 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:2 , beer_id:3 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:2 , beer_id:4 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:2 , beer_id:4 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:3 , beer_id:5 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:3 , beer_id:5 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:3 , beer_id:6 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:3 , beer_id:6 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:4 , beer_id:7 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:4 , beer_id:7 , notes:'meh' }),
-        knex('brewer_notes').insert({user_id:4 , beer_id:8 , notes:'good' }),
-        knex('brewer_notes').insert({user_id:4 , beer_id:8 , notes:'meh' })
+        knex('brewer_notes').insert({user_id:1 , beer_id:1 , notes:'tastes fine, smells awful' }),
+        knex('brewer_notes').insert({user_id:1 , beer_id:2 , notes:'big and bold' }),
+        knex('brewer_notes').insert({user_id:1 , beer_id:3 , notes:'pairs nicely with captain crunch' }),
+        knex('brewer_notes').insert({user_id:1 , beer_id:4 , notes:'a radioactive wasteland' }),
       ]);
     });
 };
