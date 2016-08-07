e3e742cd2abf40766ef1660514f58fbbd481a684
Alex Black <alexjblack322@gmail.com>
2016-08-07 16:06:51 -0600

    Wrote queries and routes for updating the finished_batches and beer_stats tables

--- queries/finished_batches.js
+++ queries/finished_batches.js
@@ -1,14 +1,19 @@
-var knex=require('../db/knex');
+var knex = require('../db/knex');
 
 module.exports = {
 
-all: function(batchId){
-  return knex.raw(`SELECT * from finished_batches WHERE batch_id = ${batchId}`)
-},
-findIngredients: function(beerId){
-  console.log(beerId);
-  return knex.raw(`SELECT ingredient_name from ingredients JOIN beer_ingredients ON beer_ingredients.ingredient_id=ingredients.id WHERE beer_id=${beerId};`)
-}
+    all: function(batchId) {
+        return knex.raw(`SELECT * from finished_batches WHERE batch_id = ${batchId}`)
+    },
+    findIngredients: function(beerId) {
+        return knex.raw(`SELECT ingredient_name from ingredients JOIN beer_ingredients ON beer_ingredients.ingredient_id=ingredients.id WHERE beer_id=${beerId};`)
+    },
+    updateBeerStats: function(stats){
+        return knex.raw(`INSERT INTO beer_stats VALUES (DEFAULT, ${stats.batch_id}, ${stats.wort_collection}, ${stats.batch_size}, ${stats.efficiency}, ${stats.gravity} )`)
+    },
+    updateFinishedBatches: function(finishedBatch){
+        return knex.raw(`INSERT INTO finished_batches VALUES (DEFAULT, '${finishedBatch.beer_name}', ${finishedBatch.user_id}, ${finishedBatch.batch_id}, '${finishedBatch.taste}', '${finishedBatch.aftertaste}', '${finishedBatch.smell}', '${finishedBatch.mouth_feel}', '${finishedBatch.drinkability}')`)
+    },
 
 
 }
